1eaa7844f8ba0b9ec0084e248c295ab665a8e37c
Nicholas Page <npage@southhills.edu>
2023-02-08 22:46:14 -0500

    Initial commit

---
+++ LINQ/LINQ/Assignment.cs
@@ -0,0 +1,81 @@
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    public class Assignment
+    {
+        private static readonly List<Student> students = DummyData.GenerateStudents();
+        private static readonly List<Teacher> teachers = DummyData.GenerateTeachers();
+        private static readonly List<Class> classes = DummyData.GenerateClasses(teachers, students);
+
+        public static void Run()
+        {
+            //(1 pt)
+            Console.WriteLine($"Teacher paid the most: {GetTeacherPaidTheMost()}");
+
+            //(1 pt)
+            Console.WriteLine($"Oldest student: {GetOldestStudent()}");
+
+            //(2 pts)
+            const float gpaTarget = 3.4f;
+            Console.WriteLine($"Oldest student: {GetStudentsWithGPAOver(gpaTarget)}");
+
+            //(2 pts)
+            Class targetClass = classes.Where(x => x.Department == Department.ComputerScience).First();
+            Console.WriteLine($"Highest GPA student in class {targetClass.ClassName}: {GetHighestGPAStudentInClass(targetClass)}");
+
+            //(3 pts)
+            const Department deptTarget = Department.ComputerScience;
+            const int numOfStudentsTarget = 11;
+            Console.WriteLine($"Classes with over {numOfStudentsTarget} students in {deptTarget}: " +
+                $"{Program.StringifyList(GetClassesInDepartmentWithOverXStudents(deptTarget, numOfStudentsTarget))}");
+
+            //(3 pts)
+            const Department deptTarget2 = Department.English;
+            Console.WriteLine($"Highest GPA student in department {deptTarget2}: {GetHighestGPAStudentInDepartment(deptTarget2)}");
+        }
+
+        //Return the teacher that is being paid the most
+        private static Teacher GetTeacherPaidTheMost()
+        {
+            return null;
+        }
+
+        //Return the oldest student
+        private static Student GetOldestStudent()
+        {
+            return null;
+        }
+
+        //Return a list of students with a GPA over the target
+        private static List<Student> GetStudentsWithGPAOver(float gpa)
+        {
+            return null;
+        }
+
+        //Return the student with the highest GPA in the target class
+        private static Student GetHighestGPAStudentInClass(Class targetClass)
+        {
+            return null;
+        }
+
+        //Return a list of classes in the target department with at least (>=) the target number of students
+        private static List<Class> GetClassesInDepartmentWithOverXStudents(Department dept, int minNumOfStudents)
+        {
+            return null;
+        }
+
+        //Returns the student with the highest GPA considering all classes in a department
+        private static Student GetHighestGPAStudentInDepartment(Department targetDept)
+        {
+            return null;
+        }
+    }
+}
---
+++ LINQ/LINQ/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class Student : Person
+    {
+        public const float HONOR_GPA = 3.5f;
+        public float GPA { get; set; }
+
+        public Student(string firstName, string lastName, int age, float GPA)
+            : base(firstName, lastName, age) => this.GPA = GPA;
+
+        public bool IsHonorStudent() => GPA >= HONOR_GPA;
+
+        public override String ToString() => base.ToString() + $" and has a GPA of {GPA:F2}";
+    }
+}
